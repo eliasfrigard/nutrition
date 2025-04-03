1607ac84ef854e851025b067cc78692ad268ed36
Elias Frigård <elias.frigard@mediatailor.fi>
2025-04-03 15:36:59 +0300

    fix remove button styling

--- components/NutritionView/NutritionItem.tsx
+++ components/NutritionView/NutritionItem.tsx
@@ -22,11 +22,11 @@ const NutritionItem = ({
         <p className='text-sm'>({quantity}g)</p>
       </div>
 
-      <div className='flex flex-col md:flex-row gap-2 h-full justify-end items-center'>
+      <div className='flex flex-col md:flex-row gap-2 h-full justify-end sm:items-center'>
         {Object.entries(foodItem.nutrition).map(([key, value]) => (
           <div
             key={key}
-            className='border-l pl-3 md:border-l-0 md:border-r border-opacity-50 md:pl-0 pr-2 border-yellow-500 justify-center items-center h-full'
+            className='border-l pl-3 md:border-l-0 md:border-r border-opacity-50 md:pl-0 pr-2 border-yellow-500 justify-center sm:items-center h-full'
           >
             <p className='capitalize text-sm'>
               {key}: {((value * quantity) / 100).toFixed(2)}
@@ -36,7 +36,7 @@ const NutritionItem = ({
 
         <button
           onClick={() => removeItem(id)}
-          className='ml-3 h-6 w-6 flex items-center justify-center bg-yellow-600 text-white rounded-full hover:bg-yellow-700 transition font-bold text-sm'
+          className='ml-3 mt-2 sm:mt-0 h-6 w-6 flex items-center justify-center bg-yellow-600 text-white rounded-full hover:bg-yellow-700 transition font-bold text-sm'
         >
           ✕
         </button>
